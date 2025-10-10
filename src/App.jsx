0d7fa0cb1bb4d7dbfade9928c752d5307dbda41c
Karina Koldaeva <karina.mila@yandex.ru>
2025-10-10 21:38:55 +0300

    style: fix style files & modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
-import './App.css';
+import styles from './app.module.css';
+import './index.css'
 
 export function App() {
 	const [value, setValue] = useState('');
@@ -29,33 +30,33 @@ export function App() {
 	}
 
 	return (
-		<div className="app">
-			<h1 className="page-heading">Ввод значения</h1>
-			<p className="no-margin-text">
+		<div className={styles.app}>
+			<h1 className={styles["page-heading"]}>Ввод значения</h1>
+			<p className={styles["no-margin-text"]}>
 				Текущее значение <code>value</code>: "
-				<output className="current-value">{value}</output>"
+				<output className={styles["current-value"]}>{value}</output>"
 			</p>
-			{error !== '' ? <div className="error">{error}</div> : ''}
-			<div className="buttons-container">
-				<button className="button" onClick={onInputButtonClick}>
+			{error !== '' ? <div className={styles.error}>{error}</div> : ''}
+			<div className={styles["buttons-container"]}>
+				<button className={styles.button} onClick={onInputButtonClick}>
 					Ввести новое
 				</button>
 				<button
-					className="button"
+					className={styles.button}
 					disabled={!isValueValid}
 					onClick={onAddButtonClick}
 				>
 					Добавить в список
 				</button>
 			</div>
-			<div className="list-container">
-				<h2 className="list-heading">Список:</h2>
+			<div className={styles["list-container"]}>
+				<h2 className={styles["list-heading"]}>Список:</h2>
 				{list.length == 0 ? (
-					<p className="no-margin-text">Нет добавленных элементов</p>
+					<p className={styles["no-margin-text"]}>Нет добавленных элементов</p>
 				) : (
-					<ul className="list">
+					<ul className={styles.list}>
 						{list.map(({ id, value }) => (
-							<li className="list-item" key={id}>
+							<li className={styles["list-item"]} key={id}>
 								<span>{value}</span>   <span>{id}</span>
 							</li>
 						))}
