88f627fe12af42554142eebe1c5adb5fd78e5546
Karina Koldaeva <karina.mila@yandex.ru>
2025-12-26 19:10:55 +0300

    add another version

--- src/components/Field/Field.jsx
+++ src/components/Field/Field.jsx
@@ -56,3 +56,45 @@ const mapDispatchToProps = (dispatch) => ({
 });
 
 export const Field = connect(mapStateToProps, mapDispatchToProps)(FieldContainer);
+
+/*
+export class FieldContainer extends Component {
+	constructor(props) {
+		super(props);
+		this.onClickCell = this.onClickCell.bind(this);
+	}
+
+	onClickCell(index) {
+		const { field, currentPlayer, isGameEnded, dispatch } = this.props;
+
+		if (field[index] || isGameEnded) return;
+
+		const newField = [...field];
+		newField[index] = currentPlayer;
+		dispatch(setField(newField));
+
+		if (checkWin(newField, currentPlayer)) {
+			dispatch(setGameEndStatus(true));
+		} else if (newField.every((cell) => cell)) {
+			dispatch(setDrawStatus(true));
+		} else {
+			const newCurrentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+			dispatch(setCurrentPlayer(newCurrentPlayer));
+		}
+	}
+
+	render() {
+		const { field } = this.props;
+
+		return <FieldLayout field={field} onClickCell={this.onClickCell} />;
+	}
+}
+
+const mapStateToProps = (state) => ({
+	field: selectField(state),
+	currentPlayer: selectCurrentPlayer(state),
+	isGameEnded: selectGameEnd(state),
+});
+
+export const Field = connect(mapStateToProps)(FieldContainer);
+*/
